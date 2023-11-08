79b56472295602f3ece49de1ee622a9ce6fb5982
mqlstam <118191161+mqlstam@users.noreply.github.com>
2023-11-08 11:35:45 +0100

    Nestjs backend update

---
+++ apps/data-api/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BackendFeaturesMealModule } from '@avans-nx-workshop/backend/features';
+
+@Module({
+  imports: [BackendFeaturesMealModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
